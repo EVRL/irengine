39df5f37ca58b85b6f3d2d0236cb8d073f31ccb5
Kelechi Nwachukwu <caesey2001@yahoo.com>
2018-12-24 23:44:21 -0500

    Initial structure of code base (#1)
    Create initial codebase

---
+++ irengine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+'''
+Basic information retrieval (IR) engine.
+'''
+
+def normalize_text():
+    pass
+
+def process_corpus():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
