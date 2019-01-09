92969ee3bffdfca2b2b529c202723a934b7c68ee
Kelechi Nwachukwu <caesey2001@yahoo.com>
2019-01-09 00:11:50 -0500

    Add tokenize function

--- irengine.py
+++ irengine.py
@@ -5,6 +5,12 @@ Basic information retrieval (IR) engine.
 '''
 
 def tokenize(text):
+    '''
+    tokenize(text) -> list
+    '''
+    t_text = text.split()
+    return t_text
+
 def normalize(t_text):
 
 def process_corpus():
