8858078e3212254fc0cd3260a6cb2152cc000f77
Kelechi Nwachukwu <caesey2001@yahoo.com>
2019-01-10 00:01:06 -0500

    feat: Add normalize function to irengine (#4)
    * Initial structure of code base
    
    * Change function name(s) from -er to -ize
    
    * Add tokenize test case
    
    * Add tokenize function
    
    * fix: Add pass to normalize
    
    * test: Add rudimentary normalize test case
    
    * docs: Fix tense of docstring and add extra space
    
    * test: Update tokenize test case
    
    * Revert "test: Update tokenize test case"
    
    This reverts commit 8c0906e70eda32acf142ee15231da93a25a77331.
    
    * feat: Add normalize function

--- test_irengine.py
+++ test_irengine.py
@@ -4,10 +4,16 @@ import irengine as test
 class TestIREngine(unittest.TestCase):
 
     def test_tokenize(self):
-        """return TOKENS if passed a str, raises exception otherwise."""
+        """returns TOKENS if passed a str, raises exception otherwise."""
         string = "how now brown crown"
         self.assertEqual(test.tokenize(string), 
                          ["how", "now", "brown", "crown"])
+
+    def test_normalize(self):
+        """returns NORMALIZED tokens."""
+        self.assertEqual(test.normalize("Test"), "test")
+        self.assertEqual(test.normalize("test"), "test")
+
 
 if __name__ == "__main__":
     unittest.main()
